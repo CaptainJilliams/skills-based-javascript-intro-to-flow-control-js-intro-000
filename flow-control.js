3439cac806fabd9b1df4641e705b7e10239a2971
Andrew Williams <1williamsaj@gmail.com>
2019-07-12 01:55:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,5 @@
 function basicTeenager(age) {
-  if (13<age<19){
-    return "You are a teenager!"
-  }
+
 }
 
 function teenager(age) {
@@ -19,3 +17,5 @@ function ternaryTeenager(age) {
 function switchAge(age) {
 
 }
+
+basicTeenager(20)
